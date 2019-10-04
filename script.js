f95cd807502a790e97a4deb53ab0b082b8819b1e
Bitha Elias <bitha.jordan@gmail.com>
2019-10-04 13:56:56 -0400

    added tags to html

--- script.js
+++ script.js
@@ -56,16 +56,14 @@ const app = {
                     noteDiv.innerHTML += `<div class="past-notes">
                     <h4>${note.title}</h4>
                     <p>${note.text}</p>
-
+                    ${this.tagsToHtml(note)}
                     </div>`
-                    this.tagsToHtml(note)
-
                 }
             })
     },
     "tagsToHtml": (note) => {
-        let htmlArray= note.tags.map(tag => `<div class="tags">${tag}</div>`)
-        console.log(htmlArray)
+        let htmlArray = note.tags.map(tag => `<div class="tags">${tag}</div>`)
+        return htmlArray.join('\n')
     },
     "main": () => {
         if (app.data.credentials.username) {
